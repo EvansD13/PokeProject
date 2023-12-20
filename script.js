a568600e2428736d3a02244eb2b001382a022546
David Evans <david@evanstmd.plus.com>
2023-12-20 20:09:42 +0000

    moves

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
 const axios = require('axios')
 const fs=require('fs/promises')
 
-async function loopThroughPokemon(pokemonArray,startIndex){
+async function loopThroughPokemon(moveArray,startIndex){
   console.log(`commencing for starting index ${startIndex}`)
-  if (pokemonArray.length<=startIndex) {
+  if (moveArray.length<=startIndex) {
     console.log('compilation complete!')
     return
   }
-  sample=pokemonArray.slice(startIndex,startIndex+25)
+  sample=moveArray.slice(startIndex,startIndex+25)
   console.log(sample)
   console.log('requesting...')
-  data= await Promise.all(sample.map((pokemon,i)=>axios.get(`https://pokeapi.co/api/v2/pokemon/${i+1+startIndex-1017+10001}`)))
+  data= await Promise.all(sample.map((move,i)=>axios.get(`https://pokeapi.co/api/v2/move/${i+1+startIndex}`)))
   console.log('reading...')
-  fs.readFile(`${__dirname}/pokemonDB.json`,'utf-8')
+  startIndex += 25
+
+  fs.readFile(`${__dirname}/moveDB.json`,'utf-8')
     .then(file=>{
       parsedFile=JSON.parse(file)
       console.log('processing new data...')
-      data.map(entry=>entry.data).forEach((pokemon,i)=>{
-        delete pokemon.game_indices
-        delete pokemon.held_items
-        pokemon.moves.forEach(move=>{
-          move.version_group_details=move.version_group_details[-1]
-        })
-        parsedFile.pokemon.push(pokemon)
+      data.map(entry=>entry.data).forEach((move,i)=>{
+        delete move.contest_combos
+        delete move.context_effect
+        delete move.contest_type
+        delete move.generation
+        delete move.names
+        delete move.past_values
+        delete move.super_contest_effect
+
+        
+        parsedFile.moves.push(move)
       })
       console.log('writing...')
-      return fs.writeFile(`${__dirname}/pokemonDB.json`,JSON.stringify(parsedFile),'utf-8')
+      return fs.writeFile(`${__dirname}/moveDB.json`,JSON.stringify(parsedFile),'utf-8')
     }).then(()=>{
       console.log('wait 30 seconds to allow server to relax :)')
-      setTimeout(()=>loopThroughPokemon(pokemonArray,startIndex+4),30000)
+      setTimeout(()=>loopThroughPokemon(moveArray,startIndex+25),30000)
     }).catch(()=>{
       console.log('error, rejected by server. Trying again in 60 seconds...')
-      setTimeout(()=>loopThroughPokemon(pokemonArray,startIndex),60000)
+      setTimeout(()=>loopThroughPokemon(moveArray,startIndex),60000)
     })
 }
 
-axios.get('https://pokeapi.co/api/v2/pokemon?limit=10000')
-  .then(res=>res.data.results.map(pokemon=>pokemon.url))
-  .then(pokemon=>{
-    return loopThroughPokemon(pokemon,0)
+axios.get('https://pokeapi.co/api/v2/move?limit=1000')
+  .then(res=>res.data.results.map(move=>move.url))
+  .then(move=>{
+    return loopThroughPokemon(move,0)
   })
